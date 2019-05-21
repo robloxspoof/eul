9f108cb3543aeb3f525fcc2b20bd8c061a708f6d
Bob McDouble <bobmcdouble3@gmail.com>
2019-05-21 18:52:22 -0400

    d

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 // Define the port to run on
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(__dirname + '/src'));
+app.use('/', express.static(__dirname + '/test'));
 
 
 // Listen for set port
